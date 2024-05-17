6aed1f285854f11d208ee4cd760d4d9424e478d2
Krishnamoorthy <121278834+krishnamoithy@users.noreply.github.com>
2024-05-17 12:33:32 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String args[]) {
+        int num=16;
+        String krishna= "iam krishna";
+        String moorthy=krishna;
+        System.out.println(num+" "+moorthy);
+        // System.out.print(krishna);
+    }
+}
