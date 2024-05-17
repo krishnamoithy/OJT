087a1e62bed652814f063410bd5ce3471ea93a26
Krishnamoorthy <121278834+krishnamoithy@users.noreply.github.com>
2024-05-17 16:42:19 +0530

    Update hello.java

--- day1/hello.java
+++ day1/hello.java
@@ -2,6 +2,6 @@ public class hello {
     public static void main(String[] args) {
         int a = 10;
         int b = 15;
-        System.out.println("a+b");
+        System.out.println(a+b);
     }}
 
