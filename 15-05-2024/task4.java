ed8d002fd2a116879a42fc832aa011b35f2a9389
Krishnamoorthy <121278834+krishnamoithy@users.noreply.github.com>
2024-05-21 09:48:46 +0530

    Add files via upload

---
+++ 15-05-2024/task4.java
@@ -0,0 +1,11 @@
+public class task4 {
+ public static void main(String[] args) {
+     int x =8;
+     if(x/2 ==0){
+        System.out.println("is even number");
+     }
+     else{
+        System.out.println("is odd number");
+     }
+ }   
+}
